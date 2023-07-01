8dcd121480a0e47aa53871c71cc9ea6013179256
DiViNE0001 <96766635+DiViNE0001@users.noreply.github.com>
2023-07-01 16:03:36 +0530

    My Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+var options = {
+    strings: ['Web Developer.', 'Freelancer.', 'Web Designer.', 'Programmer.', 'Engineer.'],
+    typeSpeed: 70,
+    backSpeed: 40,
+    loop: true
+};
+
+var typed = new Typed('#element', options);
